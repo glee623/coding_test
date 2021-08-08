60e4f373c7993b37241498b98496eb854faeaba2
DESKTOP-AR48O7J\JungHeeSun <glee623@naver.com>
2021-08-08 21:35:58 +0900

    양조장 print문 수정

--- baekjoon/11557. 양조장.py
+++ baekjoon/11557. 양조장.py
@@ -3,10 +3,6 @@ for i in range(int(input())):
     for j in range(int(input())):
         school, L = input().split()
         if int(L) > max_L:
-            print('max L',max_L)
-            print('L',L)
             max_L = int(L)
             max_school = school
-            print('max L', max_L)
-            print('L', L)
     print(max_school)
